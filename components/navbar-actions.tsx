ecfa1680ed1cc34617da5eb5075c7d241d86e542
chandra shekar <kusumachandu43@gmail.com>
2023-11-02 10:56:39 +0530

    added billboard component to the home page

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,10 +1,22 @@
 "use client"
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Button from './ui/button'
 import { ShoppingBag } from 'lucide-react'
 
 const NavbarActions = () => {
+
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, [])
+
+  if(!isMounted) {
+    return null;
+  }
+  
+
   return (
     <div className='ml-auto flex items-center gap-x-4'>
       <Button
